a428f4ee848a6ec38b14faaaae87ccf11523662d
Lefteris Lymperopoulos <lefterislib@gmail.com>
2021-04-25 22:16:20 +0300

    First commit

---
+++ SOA/soa/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Ro0t_KlM1!',
+      database: 'saas',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
