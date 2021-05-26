76e9629dceb086b1c7d564063489617ef1f2591f
NikolasAst <75013148+NikolasAst@users.noreply.github.com>
2021-05-26 09:54:33 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
   await app.listen(3000);
 }
 bootstrap();
