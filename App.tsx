64fb2f00a52a32c20fa5a964be34eb26d55f8819
NikolasAst <75013148+NikolasAst@users.noreply.github.com>
2021-07-08 18:22:28 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import CreateQuestion from "./pages/CreateQuestion"
+import Answer from './pages/Answer';
+import ChartPerDay from './pages/ChartPerDay';
+import MyQaA from './pages/MyQaA';
+import ChartPerKeyword from './pages/ChartPerKeyword';
+import ChartPerId from './pages/ChartPerId';
+
+
+function App() {
+    const [username, setUsername] = useState('');
+    const [id, setId] = useState('');
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:3006/api/user', {
+                    method: 'GET',
+                    headers: {'Content-Type': 'application/json'},
+                    credentials: 'include',
+                });
+
+                const content = await response.json();
+                setUsername(content.username);
+                setId(content.id);
+            }
+        )();
+    });
+
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Nav username={username} setUsername={setUsername}/>
+
+                <main className="form-signin">
+                    <Route path="/" exact component={() => <Home username={username}/>}/>
+                    <Route path="/login" component={() => <Login setUsername={setUsername}/>}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/question" exact component={() => <CreateQuestion id={id}/>}/>
+                    <Route path="/answer" exact component={() => <Answer id={id}/>}/>
+                    <Route path="/chart" component={ChartPerDay}/>
+                    <Route path="/chartk" component={ChartPerKeyword}/>
+                    <Route path="/charti" component={ChartPerId}/>
+                    <Route path="/qa" exact component={() => <MyQaA id={id}/>}/>
+                </main>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
