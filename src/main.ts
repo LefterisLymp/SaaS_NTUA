678b9e5ed3129cc2e485ba2b35b8f5d8096a77a4
NikolasAst <75013148+NikolasAst@users.noreply.github.com>
2021-07-10 20:29:46 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from "cookie-parser";
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  await app.use(cookieParser(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-  });
+  await app.use(cookieParser());
+  await app.use(cors());
   
   await app.listen(process.env.PORT || 3000);
 }
