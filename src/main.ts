12391a3a4bf11ba61ae79030c5b8a3e42a52e63a
NikolasAst <75013148+NikolasAst@users.noreply.github.com>
2021-07-10 19:45:52 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.use(cookieParser());
+  app.enableCors({
+    origin: 'https://service-bus.herokuapp.com/',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
