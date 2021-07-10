d8586726e19896d9176030997ca2e4b7064829ba
NikolasAst <75013148+NikolasAst@users.noreply.github.com>
2021-07-10 20:34:32 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from "cookie-parser";
-import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   await app.use(cookieParser());
-  await app.use(cors());
   
   await app.listen(process.env.PORT || 3000);
 }
