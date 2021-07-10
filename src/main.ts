82113237912c734f8964d0595cac4a68e4fb2130
NikolasAst <75013148+NikolasAst@users.noreply.github.com>
2021-07-10 20:13:36 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,15 @@ async function bootstrap() {
     origin: 'https://service-bus.herokuapp.com/',
     credentials: true,
   });
+  
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+  
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
