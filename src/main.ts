260ccdcd3176ed20a642af95a663fa3ec70ab8df
NikolasAst <75013148+NikolasAst@users.noreply.github.com>
2021-07-10 17:27:11 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.use(cookieParser());
-  var port_number = server.listen(process.env.PORT || 3000);
-  await app.listen(port_number);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
