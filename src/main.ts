c4612b9247b36263da61d834f51060b530456630
NikolasAst <75013148+NikolasAst@users.noreply.github.com>
2021-07-11 16:46:19 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
   
   await app.use(cookieParser());
 
