7837fe9b1e96e4e318da0e2a504b66a1b09e14cb
Lefteris Lymperopoulos <lefterislib@gmail.com>
2021-07-11 16:10:59 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from "cookie-parser";
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.use(cookieParser());
   await app.listen(process.env.PORT || 3000);
+  app.enableCors({ origin: 'https://frontend-soa.herokuapp.com' });
 }
 bootstrap();
